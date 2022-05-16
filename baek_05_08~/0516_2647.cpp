65ab1353014e0dafe7854597d71c7a42c272f212
BENDENG1 <76191161+BENDENG1@users.noreply.github.com>
2022-05-16 22:50:52 +0900

    0516_백준2647

---
+++ baek_05_08~/0516_2647.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+int ft_abs(int num);
+int sum_ans = 2000000000;
+int arr[1000001];
+
+int main()
+{
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+
+    int n, l, r, l_ans, r_ans, sum;
+
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    l = 0;
+    r = n - 1;
+    while (l < r)
+    {
+        sum = arr[l] + arr[r];
+        if (ft_abs(sum) < ft_abs(sum_ans))
+        {
+            sum_ans = sum;
+            l_ans = arr[l];
+            r_ans = arr[r];
+        }
+        if (sum == 0)
+            break;
+        else if (sum > 0)
+            r--;
+        else if (sum < 0)
+            l++;
+    }
+    cout << l_ans << ' ' << r_ans;
+}
+
+int ft_abs(int num)
+{
+    if (num < 0)
+        return -num;
+    else
+        return num;
+}
