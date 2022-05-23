acb3782286e8350af7fb04c06c180fd6767e7014
BENDENG1 <76191161+BENDENG1@users.noreply.github.com>
2022-05-23 21:29:59 +0900

    Create 0524_la_2567continue.cpp

---
+++ baek_05_08~/0524_la_2567continue.cpp
@@ -0,0 +1,22 @@
+/*
+왼쪽 아래의 꼭지점을 알려줌 그러면 우리는 그걸 조합해서 둘레를 측정
+(25 - 3)  * 2 + (24 - 2) * 2 = 88 에다가 안에있는 둘레 +8 = 96이다.
+*/
+
+#include <iostream>
+
+using namespace std;
+int n;
+int arr[100][100];
+
+int main()
+{
+    ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
+
+    cin>>n;
+    for(int i = 0;i<n;i++)
+    {
+
+    }
+
+}
