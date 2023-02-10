c215b6c106af05ccae42e5d96f1a8b129fa117ea
Gyun uk Roh <nogom369@naver.com>
2023-02-10 17:17:05 +0900

    2573ing

---
+++ baek_230207~/230209_2537.cpp
@@ -0,0 +1,18 @@
+
+
+#include <iostream>
+
+using namespace std;
+
+int n,m;
+
+
+int main(){
+	ios_base::sync_with_stdio(false),cin.tie(0),cout.tie(0);
+
+	cin>>n>>m;
+
+	for(int i = 0; i < n; i++){
+
+	}
+}
